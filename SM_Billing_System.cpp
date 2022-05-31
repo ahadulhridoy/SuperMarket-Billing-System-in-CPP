3bac33bf294db214b941b467aa14b725c2432e29
MD AHADUL ISLAM HRIDOY <ahadulhridoy1@gmail.com>
2022-05-31 11:10:08 +0600

    Version 0: Initial_Work_Done

---
+++ SM_Billing_System.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+
+class shopping
+{
+private:
+    int product_Code;
+    float product_Price;
+    float product_Discount;
+    std::string product_Name;
+public:
+    void Main_Menu();
+    void adminitrator();
+    void buyer();
+    void add();
+    void edit();
+    void remov();
+    void cart();
+    void receipt();
+};
